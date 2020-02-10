c5a1742c700520f1878f00d118248d57dd558319
Maksim Tymkovskiy <maksimvt@gmail.com>
2020-02-10 22:12:07 +0300

    task 18

--- Archiver.java
+++ Archiver.java
@@ -3,23 +3,10 @@ package com.javarush.task.task31.task3110;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Archiver {
 
     public static void main(String[] args) throws Exception {
-//todo test!!! убрать перед сдачей
-/*        Path path = Paths.get("C:\\JavaRushTasks\\3.JavaMultithreading\\src\\com\\javarush\\task\\task31\\task3110\\res\\archiv.zip");
-        ZipFileManager zipFileManager = new ZipFileManager(path);
-        List<Path> list = new ArrayList<>();
-                list.add(Paths.get("file.TXT"));
-                list.add(Paths.get("fil_.TXT"));
-
-        System.out.println(list.size());
-        zipFileManager.removeFiles(list);*/
 
 
         Operation operation = null;
@@ -32,7 +19,6 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-
         } while (operation != Operation.EXIT);
     }
 
